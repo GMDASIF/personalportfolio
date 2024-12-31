7b6c79bab05fbfd5a3d73b713d86930bc4c289aa
GMDASIF <gmdasif@hotmail.com>
2024-12-31 17:39:44 +0530

    add dispaly current time functionality

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
+function updateTime() {
+    const now = new Date();
+    const options = { hour: 'numeric', minute: 'numeric'}; 
+    const formattedTime = now.toLocaleString('en-US', options); 
+    document.getElementById('clock').textContent = formattedTime;
+  }
+
+  // Update the time every second
+  setInterval(updateTime, 1000);
+
+  // Initial call to display time immediately
+  updateTime();
+
+
 function downloadResume(){
     window.open("files/Resume.pdf", "_blank");
 }
